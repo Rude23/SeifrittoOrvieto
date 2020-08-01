e48e9a97282013db3710e2e16b1e840488f807dd
Rude23 <fulvio.fantacone@gmail.com>
2020-08-01 10:59:47 +0100

    ready to push

--- ordinazioni/time_extra.py
+++ ordinazioni/time_extra.py
@@ -6,18 +6,13 @@ from django.utils import timezone
 OPEN_MORNING = time(hour=12, minute=0)
 CLOSING_MORNING = time(hour=14,minute=0)
 
-OPEN_AFTERNOON = time(19,0)
+OPEN_AFTERNOON = time(17,0)
 CLOSING_AFTERNOON = time(22,0)
 
 register=template.Library()
 
 @register.filter(name="isOpen")
 def isOpen():
-
-
     now=timezone.localtime(timezone.now()).time()
-    print(timezone.localtime(timezone.now()).time())
-    print(now)
-    print(OPEN_MORNING <= now <=CLOSING_MORNING or OPEN_AFTERNOON <= now <=CLOSING_AFTERNOON)
 
     return OPEN_MORNING <= now <=CLOSING_MORNING or OPEN_AFTERNOON <= now <=CLOSING_AFTERNOON
